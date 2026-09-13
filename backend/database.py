f3f5da3567a2e27267e8f425e3608c8f570f3f29
Nitin Pachaury <nitinpachaury88@gmail.com>
2026-09-14 02:52:22 +0530

    add SSL CA cert path for Aiven MySQL

--- backend/database.py
+++ backend/database.py
@@ -5,6 +5,8 @@ from config import settings
 connect_args = {}
 if settings.DATABASE_URL.startswith("sqlite"):
     connect_args = {"check_same_thread": False}
+elif settings.DATABASE_URL.startswith("mysql"):
+    connect_args = {"ssl": {"ca": "ca.pem"}}
 
 engine = create_engine(settings.DATABASE_URL, connect_args=connect_args, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
